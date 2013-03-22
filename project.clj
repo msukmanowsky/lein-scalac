ea2c7ed677c17ceb5f4430cdf67c8fd0b52ea9e3
Tom Wanielista <tomwans@gmail.com>
2013-03-21 18:38:51 -0700

    don't depend on any version of the scala compiler, make the user specify in their project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject lein-scalac "0.1.0"
+(defproject lein-scalac "0.1.1"
   :eval-in-leiningen true
-  :dependencies [[org.scala-lang/scala-compiler "2.9.1"]
-                 [lancet "1.0.1"]])
+  :dependencies [[lancet "1.0.1"]])
