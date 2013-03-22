9f8c20727dc9f3d2508b911554c32f44f3778ba4
Tom Wanielista <tomwans@gmail.com>
2013-03-21 18:44:44 -0700

    namespace the project.

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
 (defproject io.tomw/lein-scalac "0.1.1"
+  :description "Compile Scala using Leiningen. (Fork of https://github.com/technomancy/lein-scalac)"
+
   :eval-in-leiningen true
-  :dependencies [[lancet "1.0.1"]])
+  :dependencies [[lancet "1.0.1"]]
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/tomwans/lein-scalac")
